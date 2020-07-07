9623c2c68d4f6b7b93ba9430635f64df3331a0e6
mendox74 <mendox74@gmail.com>
2020-07-07 23:02:09 +0900

    Title 画面の追加

--- game-client/title/index.js
+++ game-client/title/index.js
@@ -1,17 +1,83 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import * as Animatable from 'react-native-animatable';
-
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import Modal from "react-native-modal";
+import RigidBodies from "../app/index";
 
 export default class Title extends Component {
     constructor() {
       super();
+      this.state = {
+       inputValue: "You can change me!",
+       isMoadlVisible: false,
+       sceneVisible: false,
+       scene: null,
+      };
     }
-    
+
+    mountScene = scene => {
+      this.setState({
+        sceneVisible: true,
+        // scene: scene
+      });
+    };
+  
+    unMountScene = () => {
+      this.setState({
+        sceneVisible: false,
+        // scene: null
+      });
+    };
+
+    toggleModal = () => {
+      this.setState({ isModalVisible: !this.state.isModalVisible });
+    };
+  
+    _handleTextChange = inputValue => {
+      this.setState({ inputValue });
+    };
+
     render() {
         return(
             <View style={styles.container}>
-                <Text>Title</Text>
+                <Text style={styles.paragraph}>
+                  WATCH or TOUCH
+                </Text>
+                <Text 
+                style={styles.paragraph}
+                onPress={this.toggleModal}
+                >
+                  {this.state.inputValue}
+                </Text>
+                <Button
+                  title="GO"
+                  onPress={this.mountScene}
+                />
+                <Modal 
+                animationIn="bounceInLeft"
+                animationOut="bounceOut"
+                isVisible={this.state.isModalVisible}
+                >
+                  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#fff" }}>
+                    <Text>NAME</Text>
+                    <TextInput
+                      value={this.state.inputValue}
+                      onChangeText={this._handleTextChange}
+                      style={{ width: 200, height: 44, padding: 8, textAlign: 'center',}}
+                    />
+                    <Button
+                      title="OK"
+                      onPress={this.toggleModal}
+                    />
+                  </View>
+                </Modal>
+                <Modal
+                  animationIn="none"
+                  transparent={false}
+                  isVisible={this.state.sceneVisible}
+                  onRequestClose={_ => {}}
+                >
+                  {this.state.scene}
+                </Modal>
             </View>
         );
     }
@@ -19,6 +85,16 @@ export default class Title extends Component {
 
 const styles = StyleSheet.create({
     container: {
-      backgroundColor: '#C0E4A9',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+      margin: 24,
+      fontSize: 28,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      color: '#34495e',
     },
   });
